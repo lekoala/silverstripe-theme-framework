087e26c34d9eae7a6afc7a72acdfb3790cbe6ca6
Thomas <thomas@lekoala.be>
2016-03-23 11:52:24 +0100

    rebuild styles

---
+++ code/ThemeRebuildAllStylesTask.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Rebuild styles for all subsites
+ *
+ * @author Koala
+ */
+class ThemeRebuildAllStylesTask extends BuildTask
+{
+
+    public function run($request)
+    {
+        set_time_limit(0);
+        increase_memory_limit_to();
+        
+        Subsite::$disable_subsite_filter = true;
+
+        $mainConfig = SiteConfig::current_site_config();
+        $mainConfig->compileStyles();
+        DB::alteration_message("Compile styles for main site");
+
+        $subsites = Subsite::get();
+        foreach ($subsites as $subsite) {
+            $subsiteConfig = SiteConfig::get()->filter('SubsiteID', $subsite->ID)->first();
+            if (!$subsiteConfig) {
+                DB::alteration_message("No config for subsite ".$subsite->ID,
+                    "error");
+                continue;
+            }
+            $mainConfig->compileStyles();
+            DB::alteration_message("Compile styles for subsite ".$subsite->ID);
+        }
+        DB::alteration_message("All done");
+    }
+}
