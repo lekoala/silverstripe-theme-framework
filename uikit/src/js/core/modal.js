3de1dbaefaf6da07c2c059b4f01db9c25f82f518
Thomas <thomas@lekoala.be>
2016-04-18 09:56:58 +0200

    uikit v2.26.2

--- uikit/src/js/core/modal.js
+++ uikit/src/js/core/modal.js
@@ -4,6 +4,12 @@
 
     var active = false, activeCount = 0, $html = UI.$html, body;
 
+    UI.$win.on("resize orientationchange", UI.Utils.debounce(function(){
+        UI.$('.uk-modal.uk-open').each(function(){
+            UI.$(this).data('modal').resize();
+        });
+    }, 150));
+
     UI.component('modal', {
 
         defaults: {
@@ -44,6 +50,10 @@
                 if (target[0] == $this.element[0] && $this.options.bgclose) {
                     $this.hide();
                 }
+            });
+
+            UI.domObserve(this.element, function(e) {
+                $this.resize();
             });
         },
 
@@ -216,10 +226,6 @@
                     active.hide();
                 }
             });
-
-            UI.$win.on("resize orientationchange", UI.Utils.debounce(function(){
-                if (active) active.resize();
-            }, 150));
         },
 
         init: function() {
--- uikit/src/js/core/switcher.js
+++ uikit/src/js/core/switcher.js
@@ -45,7 +45,7 @@
 
                 this.connect = UI.$(this.options.connect);
 
-                this.connect.find(".uk-active").removeClass(".uk-active");
+                this.connect.children().removeClass("uk-active");
 
                 // delegate switch commands within container content
                 if (this.connect.length) {
@@ -98,10 +98,6 @@
                 // Init ARIA for toggles
                 toggles.not(active).attr('aria-expanded', 'false');
                 active.attr('aria-expanded', 'true');
-
-                this.on('changed.uk.dom', function() {
-                    $this.connect = UI.$($this.options.connect);
-                });
             }
 
         },
