02a89db797b614df09f2833d8b825ca7d9bca8c0
Shetza <unknownshetza@gmail.com>
2018-03-30 09:02:28 +0200

    Update ThemeRebuildAllStylesTask.php
    Subsite may not exit

--- code/ThemeRebuildAllStylesTask.php
+++ code/ThemeRebuildAllStylesTask.php
@@ -13,22 +13,24 @@ class ThemeRebuildAllStylesTask extends BuildTask
         set_time_limit(0);
         increase_memory_limit_to();
         
-        Subsite::$disable_subsite_filter = true;
+        if(class_exists('Subsite')) Subsite::$disable_subsite_filter = true;
 
         $mainConfig = SiteConfig::current_site_config();
         $mainConfig->compileStyles();
         DB::alteration_message("Compile styles for main site");
 
-        $subsites = Subsite::get();
-        foreach ($subsites as $subsite) {
-            $subsiteConfig = SiteConfig::get()->filter('SubsiteID', $subsite->ID)->first();
-            if (!$subsiteConfig) {
-                DB::alteration_message("No config for subsite ".$subsite->ID,
-                    "error");
-                continue;
+        if(class_exists('Subsite')) {
+            $subsites = Subsite::get();
+            foreach ($subsites as $subsite) {
+                $subsiteConfig = SiteConfig::get()->filter('SubsiteID', $subsite->ID)->first();
+                if (!$subsiteConfig) {
+                    DB::alteration_message("No config for subsite ".$subsite->ID,
+                        "error");
+                    continue;
+                }
+                $subsiteConfig->compileStyles();
+                DB::alteration_message("Compile styles for subsite ".$subsite->ID);
             }
-            $subsiteConfig->compileStyles();
-            DB::alteration_message("Compile styles for subsite ".$subsite->ID);
         }
         DB::alteration_message("All done");
     }
